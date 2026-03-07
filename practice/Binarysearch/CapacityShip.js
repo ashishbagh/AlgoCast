76bd8bbb90b9889d3a78645f633c2a09240877a8
Ashish <ashish_baghel@intuit.com>
2026-03-07 20:02:42 +0530

    new practic algos

--- practice/Binarysearch/CapacityShip.js
+++ practice/Binarysearch/CapacityShip.js
@@ -1,3 +1,15 @@
+// A conveyor belt has packages that must be shipped from one port to another within days days.
+
+// The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). It is not allowed to load weight more than the maximum weight capacity of the ship.
+
+// Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+
+// Example 1:
+
+// Input: weights = [2,4,6,1,3,10], days = 4
+
+// Output: 10
+
 class Solution {
   /**
    * @param {number[]} weights
